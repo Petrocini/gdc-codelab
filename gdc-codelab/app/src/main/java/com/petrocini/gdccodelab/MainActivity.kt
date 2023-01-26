e655bf98ec9c291562393c5b55bae2190eb13d7f
lucas <lucaspetrocinireis@hotmail.com>
2023-01-26 19:26:30 -0300

    first commit

---
+++ gdc-codelab/app/src/main/java/com/petrocini/gdccodelab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.petrocini.gdccodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
